3455e4a7497bf295c32d7ea4e1c78f2a9249ee39
rudy-rojas <rudy.rs1@gmail.com>
2022-09-06 14:01:43 -0400

    Listas y sus keys en react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import './main.css';
 import Button from './Button';
 
+const arr = [
+    'chanchito feliz',
+    'chanchito triste',
+    'chanchito emocionado'
+];
+
 function App(){
+
+    const myVariable = false;
+    if(myVariable) {
+        return (
+            <p>My variable is TRUE</p>
+        ); 
+    }
    return(
     <div>
         <h1>Hello World</h1>
+        {arr.map(el => <p key={el}>{el}</p> )}
         <Button onClick={() => console.log('clicked...')} >Save</Button>
+        <Button onClick={() => console.log('click cancel')} >Cancel</Button>
     </div>
    ) 
 }
--- src/Button.js
+++ src/Button.js
@@ -2,7 +2,8 @@ import './button.css';
 const Button = (props) => {
     console.log(props);
     return (
-        <button onClick={props.onClick} className="btn">nameButton={props.children}</button>
+        // <button onClick={props.onClick} className="btn">nameButton={props.children}</button>
+        <button {...props} className="btn"/>
     );
 }
 
