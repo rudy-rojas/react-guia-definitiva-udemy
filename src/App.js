dbde36d99f7e9594b15ad00ec653a65271fa25e1
rudy-rojas <rudy.rs1@gmail.com>
2022-09-06 12:01:52 -0400

    Anatomia de un componente

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
+const Li = ({ age, children, lastName }) => {
+  return (
+    // <li value={props.age}> Valor de li {props.children} | Last Name = {props.lastName}</li>
+    <li value={age}> Valor de li {children} | Last Name = {lastName}</li>
+
+  )
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ol>
+      <Li
+        age={3}
+        status={true}
+        lastName={'Rojas'}
+      >This is a item</Li>
+      <Li age={1}>
+        This a second item
+      </Li>
+    </ol>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ const X = () => {
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <X />
+    <App />
   </React.StrictMode>
 );
 
